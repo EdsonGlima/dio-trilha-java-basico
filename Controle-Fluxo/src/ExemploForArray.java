4afbd049247b4ab7c319486796807b9f908b1088
EdsonGlima <edsonglima7199@yahoo.com>
2024-09-21 18:53:34 -0300

    exemplos de for break e for array

---
+++ Controle-Fluxo/src/ExemploForArray.java
@@ -0,0 +1,8 @@
+public class ExemploForArray {
+    public static void main(String[] args) {
+        String alunos[]={"FELIPE","JONAS","JULIA","RAMOS","MARCOS"};
+        for(int x=0; x < alunos.length; x++){
+            System.out.println("O ALUNO DO INDICE X = " + x + " É "  + alunos[x]);
+        }
+    }
+}
