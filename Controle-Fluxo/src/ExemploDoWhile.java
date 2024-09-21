10c93a5fcd75911388399f78c6c4f6837d330472
EdsonGlima <edsonglima7199@yahoo.com>
2024-09-21 20:29:26 -0300

    ExemploDoWhile

---
+++ Controle-Fluxo/src/ExemploDoWhile.java
@@ -0,0 +1,15 @@
+import java.util.Random;
+public class ExemploDoWhile {
+    public static void main(String[] args) {
+        System.out.println("DISCANDO...");
+        do {
+            System.out.println("TELEFONE TOCANDO!");
+        }while(tocando());
+        System.out.println("ALÔ !!!");
+    }
+    private static boolean tocando(){
+        boolean atendeu = new Random().nextInt(3)==1;
+        System.out.println("ATENDEU? " + atendeu);
+        return ! atendeu;
+    }
+}
